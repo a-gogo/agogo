9d189874b663fb4a4bafb2610a3a04ef2689c016
Max Burri <burri@puzzle.ch>
2021-01-26 15:21:09 +0100

    Revert "fix intellij warning"
    This reverts commit 01355f0aefd23ccd65af764519b5db90729c8b0f.

--- AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/deployments/DeploymentsRest.java
+++ AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/deployments/DeploymentsRest.java
@@ -136,7 +136,7 @@ public class DeploymentsRest {
         List<DeploymentDTO> deploymentDTOs = createDeploymentDTOs(filteredDeployments);
         return Response.status(Status.OK)
                        .header("X-Total-Count", filteredDeployments.getB())
-                       .entity(new GenericEntity<>(deploymentDTOs) {
+                       .entity(new GenericEntity<List<DeploymentDTO>>(deploymentDTOs) {
                        })
                        .build();
     }
@@ -363,7 +363,7 @@ public class DeploymentsRest {
                            .build();
         }
 
-        String logfileContent;
+        String logfileContent = "";
         try {
             logfileContent = deploymentBoundary.getDeploymentLog(fileName);
         } catch (IllegalAccessException e) {
@@ -441,7 +441,7 @@ public class DeploymentsRest {
         ResourceEntity appServer;
         Set<ResourceEntity> apps;
         ContextEntity environment = null;
-        List<ApplicationWithVersion> applicationsWithVersion;
+        List<ApplicationWithVersion> applicationsWithVersion = new ArrayList<>();
         LinkedList<CustomFilter> filters = new LinkedList<>();
         ReleaseEntity release;
         ResourceGroupEntity resourceGroupEntity;
@@ -568,7 +568,7 @@ public class DeploymentsRest {
             applicationsWithVersion = convertToApplicationWithVersion(request.getAppsWithVersion(), apps);
         } else {
             applicationsWithVersion = deploymentBoundary.getVersions(appServer,
-                                                                     new ArrayList<>(environment.getId()),
+                                                                     new ArrayList<Integer>(environment.getId()),
                                                                      release);
         }
         return applicationsWithVersion;
